451cbeda2563c9ed1b7c28b389623a00c46acc5c
SatoTwo <kosei.iidx@gmail.com>
2024-03-02 15:00:42 +0900

    change port

--- src/app.py
+++ src/app.py
@@ -87,4 +87,4 @@ def visualize(seed: int, output: str):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, host="0.0.0.0")
+    app.run(debug=True, use_reloader=True, host="0.0.0.0", port="8080")
