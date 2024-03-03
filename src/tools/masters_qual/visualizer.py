40b9eb85e3a2041428392642e4fbb70be2d88d86
SatoTwo <kosei.iidx@gmail.com>
2024-03-03 14:24:08 +0900

    update visualizer

--- src/tools/masters_qual/visualizer.py
+++ src/tools/masters_qual/visualizer.py
@@ -10,9 +10,15 @@ def int_to_color(a: int, N: int) -> str:
     return f"rgb({r}, 0, {b})"
 
 
-def get_input_visualizer(input: Input) -> go.Figure:
-    fig = go.Figure()
+def add_grid(fig: go.Figure, input: Input) -> go.Figure:
+    N = input.N
+    for i in range(N - 1):
+        fig.add_vline(x=i + 1, opacity=0.2)
+        fig.add_hline(y=i + 1, opacity=0.2)
+    return fig
 
+
+def add_wall(fig: go.Figure, input: Input) -> go.Figure:
     N = input.N
     for i in range(N):
         for j in range(N - 1):
@@ -44,10 +50,15 @@ def get_input_visualizer(input: Input) -> go.Figure:
                 )
             )
 
+    return fig
+
+
+def add_number(fig: go.Figure, a: list[list[int]]) -> go.Figure:
+    N = len(a)
     N2 = N * N
     for i in range(N):
         for j in range(N):
-            aij = input.a[i][j]
+            aij = a[i][j]
             color = int_to_color(aij, N2)
             fig.add_trace(
                 go.Scatter(
@@ -56,13 +67,38 @@ def get_input_visualizer(input: Input) -> go.Figure:
                     mode="lines",
                     line=dict(color=color, width=0),
                     fill="toself",
+                    hoverinfo="skip",
                 )
             )
+            fig.add_trace(
+                go.Scatter(
+                    x=[j + 0.5],
+                    y=[i + 0.5],
+                    opacity=0.0,
+                    line=dict(color=color),
+                    hovertemplate=aij,
+                )
+            )
+    return fig
 
-    for i in range(N - 1):
-        fig.add_vline(x=i + 1, opacity=0.2)
-        fig.add_hline(y=i + 1, opacity=0.2)
-    fig.update_layout(width=500, height=500, showlegend=False)
+
+def update_layout(fig: go.Figure, N: int) -> go.Figure:
+    fig.update_layout(width=600, height=600, showlegend=False)
     fig.update_xaxes(range=(0, N))
     fig.update_yaxes(range=(N, 0))
     return fig
+
+
+def get_input_visualizer(input: Input) -> go.Figure:
+    fig = go.Figure()
+
+    fig = add_wall(fig, input)
+    fig = add_number(fig, input.a)
+
+    fig = add_grid(fig, input)
+    fig = update_layout(fig, input.N)
+    return fig
+
+
+def get_output_visualizer(input: Input, output: Output) -> go.Figure:
+    pass
