8e3d99ba6bec32f49e28a5ade81222ea281f4b3c
kosei <kosei.iidx@gmail.com>
2024-04-14 23:29:38 +0900

    ahc032

--- wasm/src/io.rs
+++ wasm/src/io.rs
@@ -1,30 +1,259 @@
+use modint::ModInt;
 use proconio::{input, source::auto::AutoSource};
 pub type Score = u64;
 
-pub struct Input {}
+pub struct Input {
+    pub n: usize,
+    pub m: usize,
+    pub k: usize,
+    pub a: Vec<Vec<ModInt>>,
+    pub s: Vec<Vec<Vec<ModInt>>>,
+}
 
 impl Input {
     pub fn parse_input(input_str: &str) -> Result<Self, String> {
         let source = AutoSource::from(input_str);
         input! {
             from source,
+        (n, m, k): (usize, usize, usize),
+        a_: [[u128; n]; n],
+        s_: [[[u128; 3]; 3]; m],
         }
+
+        let mut a = vec![vec![ModInt::new(0); n]; n];
+        let mut s = vec![vec![vec![ModInt::new(0); 3]; 3]; m];
+
+        for i in 0..n {
+            for j in 0..n {
+                a[i][j] = ModInt::new(a_[i][j]);
+            }
+        }
+        for i in 0..m {
+            for j in 0..3 {
+                for k in 0..3 {
+                    s[i][j][k] = ModInt::new(s_[i][j][k]);
+                }
+            }
+        }
+
+        Ok(Self { n, m, k, a, s })
     }
 }
 
-pub struct Output {}
+pub struct Output {
+    l: usize,
+    actions: Vec<(usize, usize, usize)>,
+}
 
 impl Output {
     pub fn parse_output(output_str: &str) -> Result<Self, String> {
         let source = AutoSource::from(output_str);
         input! {
             from source,
+        l: usize,
+        actions: [(usize, usize, usize); l],
+        }
+
+        Ok(Self { l, actions })
+    }
+
+    pub fn get_max_turn(&self, input: &Input) -> usize {
+        self.l
+    }
+
+    pub fn get_state_at_turn(&self, input: &Input, turn: usize) -> Vec<Vec<u64>> {
+        let mut a = input.a.clone();
+        for &(i, x, y) in self.actions.iter().take(turn) {
+            self.stamp(&mut a, &input.s[i], x, y);
+        }
+
+        let mut ret = vec![vec![0; input.n]; input.n];
+        for i in 0..input.n {
+            for j in 0..input.n {
+                ret[i][j] = a[i][j].0 as u64;
+            }
+        }
+        ret
+    }
+
+    fn stamp(&self, a: &mut Vec<Vec<ModInt>>, s: &Vec<Vec<ModInt>>, x: usize, y: usize) {
+        for i in 0..3 {
+            for j in 0..3 {
+                a[x + i][y + j] += s[i][j];
+            }
         }
     }
 
-    pub fn get_max_turn(&self, input: &Input) -> usize {}
+    pub fn calc_score(&self, input: &Input, turn: usize) -> Result<Score, String> {
+        let a = self.get_state_at_turn(input, turn);
+        let score = a
+            .into_iter()
+            .map(|v| v.into_iter().sum::<u64>())
+            .into_iter()
+            .sum();
 
-    pub fn get_state_at_turn(&self, input: &Input, turn: usize) -> () {}
+        Ok(score)
+    }
+}
 
-    pub fn calc_score(&self, input: &Input, turn: usize) -> Result<Score, String> {}
+pub mod modint {
+    use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+    type Int = u128;
+
+    #[derive(Clone, Copy)]
+    pub struct ModInt(pub Int);
+
+    impl ModInt {
+        const MOD: Int = 998244353;
+
+        pub fn new(x: Int) -> Self {
+            Self(x % Self::MOD)
+        }
+
+        pub fn add(&self, other: Self) -> Self {
+            let mut val = self.0 + other.0;
+            if val >= Self::MOD {
+                val -= Self::MOD;
+            }
+            Self(val)
+        }
+
+        pub fn sub(&self, other: Self) -> Self {
+            let val = if self.0 >= other.0 {
+                self.0 - other.0
+            } else {
+                Self::MOD - other.0 + self.0
+            };
+            Self(val)
+        }
+
+        pub fn mul(&self, other: Self) -> Self {
+            Self((self.0 * other.0) % Self::MOD)
+        }
+
+        pub fn div(&self, other: Self) -> Self {
+            self.mul(other.inv())
+        }
+
+        pub fn pow(&self, mut n: Int) -> Self {
+            let mut val = 1;
+            let mut pow = self.0;
+            while n > 0 {
+                if (n & 1) == 1 {
+                    val = (val * pow) % Self::MOD;
+                }
+                pow = (pow * pow) % Self::MOD;
+                n >>= 1;
+            }
+            Self(val)
+        }
+
+        pub fn inv(&self) -> Self {
+            self.pow(Self::MOD - 2)
+        }
+    }
+
+    impl Add for ModInt {
+        type Output = ModInt;
+        fn add(self, rhs: Self) -> Self::Output {
+            ModInt::add(&self, rhs)
+        }
+    }
+
+    impl Sub for ModInt {
+        type Output = ModInt;
+        fn sub(self, rhs: Self) -> Self::Output {
+            ModInt::sub(&self, rhs)
+        }
+    }
+
+    impl Mul for ModInt {
+        type Output = ModInt;
+        fn mul(self, rhs: Self) -> Self::Output {
+            ModInt::mul(&self, rhs)
+        }
+    }
+
+    impl Div for ModInt {
+        type Output = ModInt;
+        fn div(self, rhs: Self) -> Self::Output {
+            ModInt::div(&self, rhs)
+        }
+    }
+
+    impl Add<Int> for ModInt {
+        type Output = ModInt;
+        fn add(self, rhs: Int) -> Self::Output {
+            ModInt::add(&self, Self::new(rhs))
+        }
+    }
+
+    impl Sub<Int> for ModInt {
+        type Output = ModInt;
+        fn sub(self, rhs: Int) -> Self::Output {
+            ModInt::sub(&self, Self::new(rhs))
+        }
+    }
+
+    impl Mul<Int> for ModInt {
+        type Output = ModInt;
+        fn mul(self, rhs: Int) -> Self::Output {
+            ModInt::mul(&self, Self::new(rhs))
+        }
+    }
+
+    impl Div<Int> for ModInt {
+        type Output = ModInt;
+        fn div(self, rhs: Int) -> Self::Output {
+            ModInt::div(&self, Self::new(rhs))
+        }
+    }
+
+    impl AddAssign<ModInt> for ModInt {
+        fn add_assign(&mut self, rhs: ModInt) {
+            *self = self.add(rhs);
+        }
+    }
+
+    impl SubAssign<ModInt> for ModInt {
+        fn sub_assign(&mut self, rhs: ModInt) {
+            *self = self.sub(rhs);
+        }
+    }
+
+    impl MulAssign<ModInt> for ModInt {
+        fn mul_assign(&mut self, rhs: ModInt) {
+            *self = self.mul(rhs);
+        }
+    }
+
+    impl DivAssign<ModInt> for ModInt {
+        fn div_assign(&mut self, rhs: ModInt) {
+            *self = self.div(rhs);
+        }
+    }
+
+    impl AddAssign<Int> for ModInt {
+        fn add_assign(&mut self, rhs: Int) {
+            *self = self.add(rhs);
+        }
+    }
+
+    impl SubAssign<Int> for ModInt {
+        fn sub_assign(&mut self, rhs: Int) {
+            *self = self.sub(rhs);
+        }
+    }
+
+    impl MulAssign<Int> for ModInt {
+        fn mul_assign(&mut self, rhs: Int) {
+            *self = self.mul(rhs);
+        }
+    }
+
+    impl DivAssign<Int> for ModInt {
+        fn div_assign(&mut self, rhs: Int) {
+            *self = self.div(rhs);
+        }
+    }
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use svg::Document;
 use wasm_bindgen::prelude::*;
 mod element;
@@ -6,7 +7,42 @@ mod io;
 mod rnd;
 
 #[wasm_bindgen]
-pub fn gen(seed: i32) -> String {}
+pub fn gen(seed: i32) -> String {
+    let mut rng = rnd::RNG::new(seed as u64);
+    let n = rng.gen_range(9, 9, true);
+    let m = rng.gen_range(20, 20, true);
+    let k = rng.gen_range(81, 81, true);
+    let mut a = vec![vec![0; n]; n];
+    let mut s = vec![vec![vec![0; 3]; 3]; m];
+
+    for i in 0..n {
+        for j in 0..n {
+            a[i][j] = rng.gen_range(0, 998244353, false);
+        }
+    }
+    for i in 0..m {
+        for j in 0..3 {
+            for k in 0..3 {
+                s[i][j][k] = rng.gen_range(0, 998244353, false);
+            }
+        }
+    }
+
+    let mut input = format!("{} {} {}", n, m, k) + "\n";
+
+    for i in 0..n {
+        input += &a[i].iter().join(" ");
+        input += &"\n"
+    }
+    for i in 0..m {
+        for j in 0..3 {
+            input += &s[i][j].iter().join(" ");
+            input += &"\n";
+        }
+    }
+
+    input
+}
 
 #[wasm_bindgen(getter_with_clone)]
 pub struct Ret {
@@ -37,13 +73,56 @@ pub fn vis(input: String, output: String, turn: usize) -> Ret {
     }
 }
 
-const SVG_SIZE: usize = 600;
+const SVG_SIZE: usize = 603;
 
 fn get_svg_string(input: io::Input, output: io::Output, turn: usize) -> String {
     let mut svg = Document::new()
         .set("viewBox", (0, 0, SVG_SIZE, SVG_SIZE))
         .set("width", SVG_SIZE)
         .set("height", SVG_SIZE);
+
+    let n = input.n;
+    let d = SVG_SIZE / n;
+    let a = output.get_state_at_turn(&input, turn);
+
+    for i in 0..n {
+        for j in 0..n {
+            svg = svg.add(element::rectangle(
+                j * d,
+                i * d,
+                d,
+                d,
+                &element::color(a[i][j] as f64 / 998244353f64),
+            ));
+        }
+    }
+
+    for i in 0..n + 1 {
+        let x1 = 0;
+        let x2 = SVG_SIZE;
+        let y1 = i * d;
+        let y2 = y1;
+        let color = "black";
+        svg = svg.add(element::line(x1, y1, x2, y2, color))
+    }
+    for i in 0..n + 1 {
+        let y1 = 0;
+        let y2 = SVG_SIZE;
+        let x1 = i * d;
+        let x2 = x1;
+        let color = "black";
+        svg = svg.add(element::line(x1, y1, x2, y2, color))
+    }
+    for i in 0..n {
+        for j in 0..n {
+            svg = svg.add(element::text(
+                j * d + d / 2,
+                i * d + d / 2,
+                &a[i][j].to_string(),
+                10,
+            ));
+        }
+    }
 
     svg.to_string()
 }
